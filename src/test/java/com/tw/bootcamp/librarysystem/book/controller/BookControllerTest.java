6b0e1a12bb8a1502d70bf2133566bbccaa4a3f3e
Karthik Natarajan <karthik.natarajan@thoughtworks.com>
2020-03-27 17:20:09 +0530

    Modifying the status code for Exception scenarios

--- src/test/java/com/tw/bootcamp/librarysystem/book/controller/BookControllerTest.java
+++ src/test/java/com/tw/bootcamp/librarysystem/book/controller/BookControllerTest.java
@@ -87,7 +87,7 @@ public class BookControllerTest {
                 .willThrow(new BookNotFoundException());
 
         bookControllerMock.perform(MockMvcRequestBuilders.get("/library-system/books/1222"))
-                .andExpect(status().isBadRequest())
+                .andExpect(status().isOk())
                 .andExpect(jsonPath("message", is("Book Id requested is not present.")));
 
     }
